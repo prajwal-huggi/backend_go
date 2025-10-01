23267e33ab5b56cfe2cff4881fc65e85ff1f6cb8
prajwal-huggi <prajwalhuggi02@gmail.com>
2025-10-01 11:16:03 -0400

    Gracefully shutting down the server

--- README.md
+++ README.md
@@ -3,3 +3,6 @@ brew install go
 from the link(go lang clean)-> https://github.com/ilyakaznacheev/cleanenv
 
 go get -u github.com/ilyakaznacheev/cleanenv
+
+go run cmd/server/main.go -config config/local.yaml
+-config is the flag which is must
--- go_crud/cmd/server/main.go
+++ go_crud/cmd/server/main.go
@@ -1,7 +1,64 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"log"
+	"log/slog"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/prajwal-huggi/backend_go/internal/config"
+)
 
 func main(){
-	fmt.Println("Hello")
+	// 1) load config
+	cfg:= config.MustLoad()
+
+	// 2) database setup
+	// 3) setup router
+	router:= http.NewServeMux()
+
+	//The below is our first endpoint
+	router.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request){
+		w.Write([]byte("Welcome to CRUd operations in golang"))
+	})
+
+	// 4) setup server
+	server:= http.Server{
+		Addr: cfg.HTTPServer.Addr,
+		Handler: router,
+	}
+
+	slog.Info("Server Started: ", slog.String("address", cfg.HTTPServer.Addr) )
+
+	done:= make(chan os.Signal, 1)
+
+	signal.Notify(done , os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+
+	go func(){
+		err:= server.ListenAndServe()
+		if err!= nil{
+			log.Fatal("Failed to start the server ")
+		}
+	}()
+
+	<- done
+
+	slog.Info("Shutting down the server")
+
+	ctx, cancel:=context.WithTimeout(context.Background(), 5* time.Second)
+	defer cancel()
+
+
+	err:= server.Shutdown(ctx)
+	if err!= nil{
+		slog.Error("Failed to shutdown server", slog.String("error", err.Error()))
+	}
+
+	slog.Info("Server Shutdown Successfully ")
+
+	// The above shutdown method is known as the graceful shutdown.
 }
